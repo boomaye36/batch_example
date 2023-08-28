ab836367f4709e8df49aea955a94ccb43068c078
HyunGeon <timxxz@naver.com>
2023-08-28 23:43:03 +0900

    first

---
+++ src/main/java/com/example/spring_batch_ex/SpringBatchExApplication.java
@@ -0,0 +1,14 @@
+package com.example.spring_batch_ex;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@EnableBatchProcessing
+@SpringBootApplication
+public class SpringBatchExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring_batch_ex/SpringBatchExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_batch_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
